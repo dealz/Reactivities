eb780b40d9e9b221060f0f87c2c76d51a902e3aa
Allan Palomares <Allan.Palomares@us.fincantieri.com>
2024-08-08 15:39:37 -0500

    Section 22-252

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,7 +3,7 @@ import './styles.css'
 import {  Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite'
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import { ToastContainer } from 'react-toastify';
 import { useStore } from '../stores/store';
@@ -28,6 +28,7 @@ if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
 
 return (
     <>
+         <ScrollRestoration/>
          <ModalContainer />
          <ToastContainer position='bottom-right'  hideProgressBar theme='colored' />
          {location.pathname === '/' ? <HomePage/> : (
